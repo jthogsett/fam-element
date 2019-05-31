2341efd28ffae639f5baa1816358263130dd1d18
jthogsett <jthogsett@gmail.com>
2019-05-31 12:54:05 -0700

    feat(updateable): enable batch updates of a class
    decorate a class with onUpdate to register a callback that will be asynchronously ran after an
    update is requested against an instance

---
+++ src/updateable.ts
@@ -0,0 +1,42 @@
+const UPDATE = Symbol('update')
+const UPDATE_TASK = Symbol('updateTask')
+
+interface Updateable {
+  [UPDATE]: () => void
+  [UPDATE_TASK]?: Promise<void>
+}
+
+export function onUpdate<Target>(
+  updateCallback: (target: Target) => void
+): <TargetConstructor extends Constructor<Target>>(targetClass: TargetConstructor) => void {
+  return <TargetConstructor extends Constructor<Target & Partial<Updateable>>>(
+    targetClass: TargetConstructor
+  ) => {
+    const superUpdate = targetClass.prototype[UPDATE]
+    if (superUpdate) {
+      targetClass.prototype[UPDATE] = function() {
+        superUpdate.call(this)
+        updateCallback(this)
+      }
+    } else {
+      targetClass.prototype[UPDATE] = function() {
+        updateCallback(this)
+      }
+    }
+  }
+}
+
+const microtask = Promise.resolve()
+
+export function requestUpdate(target: any): void {
+  if (isUpdateable(target) && !target[UPDATE_TASK]) {
+    target[UPDATE_TASK] = microtask.then(() => {
+      delete target[UPDATE_TASK]
+      target[UPDATE]()
+    })
+  }
+}
+
+function isUpdateable(target: any): target is Updateable {
+  return UPDATE in target
+}
---
+++ test/updateable.test.ts
@@ -0,0 +1,54 @@
+import { onUpdate, requestUpdate } from '../src/updateable'
+
+describe('onUpdate', () => {
+  it('asynchronously calls the updateCallback when an update is requested', async () => {
+    const updateCallback = jest.fn()
+
+    @onUpdate(updateCallback)
+    class Foo {}
+
+    const test = new Foo()
+    requestUpdate(test)
+
+    expect(updateCallback).not.toBeCalled()
+    await Promise.resolve()
+    expect(updateCallback).toBeCalled()
+  })
+  it('inherits any previously registered updateCallbacks', async () => {
+    const updateCallback1 = jest.fn()
+    const updateCallback2 = jest.fn()
+
+    @onUpdate(updateCallback1)
+    @onUpdate(updateCallback2)
+    class Foo {}
+
+    const test = new Foo()
+    requestUpdate(test)
+
+    await Promise.resolve()
+    expect(updateCallback1).toBeCalled()
+    expect(updateCallback2).toBeCalled()
+  })
+})
+
+describe('requestUpdate', () => {
+  it('does nothing if there are no registered callbacks', async () => {
+    const test = {}
+    requestUpdate(test)
+    await Promise.resolve()
+  })
+  it('triggers an update once if called multiple times in the same tick', async () => {
+    const updateCallback = jest.fn()
+
+    @onUpdate(updateCallback)
+    class Foo {}
+
+    const test = new Foo()
+    requestUpdate(test)
+    requestUpdate(test)
+
+    expect(updateCallback).not.toBeCalled()
+    await Promise.resolve()
+    expect(updateCallback).toBeCalledTimes(1)
+  })
+})
