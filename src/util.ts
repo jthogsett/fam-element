4cca09e1865b5f31d906fc690d581336a265378a
jthogsett <jthogsett@gmail.com>
2019-06-01 15:13:47 -0700

    refactor(util): add property initialization utils
    added abilitiy to memoize properties against objects.  Will be used in future features

--- src/util.ts
+++ src/util.ts
@@ -16,3 +16,36 @@ export function getPropertyDescriptor<Target, Property extends keyof Target>(
   }
   return undefined
 }
+
+export function memoizeMap<Key, Value, Property extends PropertyKey>(
+  target: Partial<Record<Property, Map<Key, Value>>>,
+  property: Property
+) {
+  memoize<Record<Property, Map<Key, Value>>, Property>(
+    target,
+    property,
+    () => new Map<Key, Value>(),
+    parentMap => new Map(parentMap!.entries())
+  )
+}
+
+export function memoize<Target extends {}, Property extends keyof Target>(
+  target: Partial<Target>,
+  property: Property,
+  initialize: () => Target[Property],
+  cloneFromSuper: (superValue: Target[Property]) => Target[Property]
+) {
+  initializeOwnProperty(target, property, () =>
+    property in target ? cloneFromSuper(target[property] as Target[Property]) : initialize()
+  )
+}
+
+export function initializeOwnProperty<Target extends {}, Property extends keyof Target>(
+  target: Partial<Target>,
+  property: Property,
+  initialize: () => Target[Property]
+) {
+  if (!target.hasOwnProperty(property)) {
+    target[property] = initialize()
+  }
+}
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,4 +1,4 @@
-import { getPropertyDescriptor } from '../src/util'
+import { getPropertyDescriptor, memoize, initializeOwnProperty, memoizeMap } from '../src/util'
 
 describe('getPropertyDescriptor', () => {
   it("gets an object's property descriptor", () => {
@@ -16,5 +16,66 @@ describe('getPropertyDescriptor', () => {
     const foo = Object.create(fooSuper)
     const descriptor = getPropertyDescriptor(foo, 'bar')
     expect(descriptor).toEqual(Object.getOwnPropertyDescriptor(fooSuper, 'bar'))
+  })
+})
+
+describe('initializeOwnProperty', () => {
+  it('set the property value of an object that does not have the property', () => {
+    const foo: { bar?: string } = {}
+    initializeOwnProperty(foo, 'bar', () => 'baz')
+    expect(foo.bar).toEqual('baz')
+  })
+  it('does not set the property value if the object has the property', () => {
+    const foo: { bar?: string } = { bar: 'baz' }
+    initializeOwnProperty(foo, 'bar', () => 'qux')
+    expect(foo.bar).toEqual('baz')
+  })
+})
+
+describe('memoize', () => {
+  const initialize = jest.fn<string, []>()
+  const cloneFromSuper = jest.fn<string, [string]>()
+  interface Foo {
+    bar: string
+  }
+
+  beforeEach(() => {
+    initialize.mockClear()
+    initialize.mockImplementation(() => 'qux')
+    cloneFromSuper.mockClear()
+    cloneFromSuper.mockImplementation(superValue => `inherited ${superValue}`)
+  })
+  it("clones a property value from an object's prototype into its own property", () => {
+    const fooSuper: Foo = { bar: 'baz' }
+    const foo = Object.create(fooSuper) as Foo
+    memoize(foo, 'bar', initialize, cloneFromSuper)
+    expect(foo.bar).toEqual('inherited baz')
+  })
+  it("initializes a property value if it doesn't exist in its prototype hierarchy", () => {
+    const foo: Partial<Foo> = {}
+    memoize(foo, 'bar', initialize, cloneFromSuper)
+    expect(foo.bar).toEqual('qux')
+  })
+})
+
+describe('memoizeMap', () => {
+  it("initializes a new Map on an object if it doesn't exist", () => {
+    const fooSuper = {
+      bar: new Map<string, number>([['a', 1], ['b', 2]])
+    }
+    const foo: {
+      bar?: Map<string, number>
+    } = {}
+    memoizeMap(foo, 'bar')
+    expect(foo.bar).toStrictEqual(new Map<string, number>())
+  })
+  it("clones a Map from an object's prototype", () => {
+    const fooSuper = {
+      bar: new Map<string, number>([['a', 1], ['b', 2]])
+    }
+    const foo = Object.create(fooSuper) as typeof fooSuper
+    memoizeMap(foo, 'bar')
+    expect(foo.bar).toStrictEqual(new Map<string, number>([['a', 1], ['b', 2]]))
+    expect(foo.bar).not.toBe(fooSuper.bar)
   })
 })
